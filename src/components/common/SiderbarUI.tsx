1e75d8f380cde0ad1a11c0c60c08957fec5aa61a
Siva <72605222+Siva-555@users.noreply.github.com>
2025-10-26 22:37:48 +0530

    fix: backdrop

--- src/components/common/SiderbarUI.tsx
+++ src/components/common/SiderbarUI.tsx
@@ -132,7 +132,7 @@ export function SidebarUI() {
             exit={{ opacity: 0 }}
             transition={{ duration: 0.4 }}
             className={cn(
-              "absolute top-0 right-0 left-0 bottom-0 bg-black z-[5] w-full",
+              "fixed top-0 right-0 left-0 bottom-0 bg-black z-[5] w-full",
               // "w-[calc(100%-300px)]"
             )}
           />
