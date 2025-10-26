e058d83ba14c324977319bbb82b64d2d3b0d2233
Siva <72605222+Siva-555@users.noreply.github.com>
2025-10-26 22:39:17 +0530

    SEO optimization

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -16,11 +16,44 @@ const robotoMono = Roboto_Mono({
   variable: "--font-roboto-mono",
 });
 
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+
 export const metadata: Metadata = {
   title: "RetailEase - Your Retail Management Solution",
   description:
     "Manage your retail business with ease using RetailEase. Streamline operations, track sales, and enhance customer experience.",
-  keywords: "retailease, ration shop,  inventory management, billing system, shop management, POS system",
+  keywords: [
+    "RetaileEase",
+    "Ration Shop",
+    "Inventory Management",
+    "Billing System",
+    "Shop Management",
+    "POS system"
+  ],
+  openGraph: {
+    title: "RetailEase - Simplify Your Shop Management",
+    description:
+      "Smart billing and inventory management system for ration shops and small retailers.",
+    url: `${BASE_URL}`,
+    siteName: "RetailEase",
+    images: [
+      {
+        url: `${BASE_URL}/og-image.png`, 
+        width: 1200,
+        height: 630,
+        alt: "RetailEase - Smart Retail Management",
+      },
+    ],
+    locale: "en_IN",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "RetailEase - Your Retail Management Solution",
+    description:
+      "Streamline your shop operations with billing, inventory, and analytics tools.",
+    images: [`${BASE_URL}/og-image.png`],
+  },
 };
 
 export default async function RootLayout({
